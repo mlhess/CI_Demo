2c9df322af05d44f6c9e080664463ef41d365e56
Michael Hess <mlhess@umich.edu>
2017-01-13 18:43:47 -0500

    fix url

--- test.pl
+++ test.pl
@@ -1,5 +1,6 @@
 use Lingua::EN::Fathom;
 $repo =  $ENV{'CIRCLE_PROJECT_REPONAME'};
-my $text = Lingua::EN::Fathom->new('/home/ubuntu/' .$repo . 'all.md');
+echo $repo;
+$text = Lingua::EN::Fathom->new('/home/ubuntu/' .$repo . '/all.md');
 print($text->report);
        
