a2e7c534b47354bd3dac87f0541de1f0d2d6b303
Michael Hess <mlhess@umich.edu>
2017-01-13 18:45:32 -0500

    echo not in php

--- test.pl
+++ test.pl
@@ -1,6 +1,6 @@
 use Lingua::EN::Fathom;
 $repo =  $ENV{'CIRCLE_PROJECT_REPONAME'};
-echo $repo;
+print  $repo;
 $text = Lingua::EN::Fathom->new('/home/ubuntu/' .$repo . '/all.md');
 print($text->report);
        
