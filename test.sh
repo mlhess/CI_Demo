29876d788a008e645ecfe0819d4a94961028e639
Michael Hess <mlhess@umich.edu>
2017-01-14 11:13:24 -0500

    clean up files and do a better test

--- test.sh
+++ test.sh
@@ -1,6 +1,8 @@
-for i in `ls`; do  
-   echo $i; 
-   if (( `readability $i| grep Kincaid | sed 's/[^0-9]*//g' ` > 3080 )) then 
-     exit 2; 
-   fi 
+for i in $(ls); do
+   echo $i
+   readability $i
+   score=`readability $i| grep Kincaid | sed 's/[^0-9]*//g' `
+   if (( score < 2000 )); then
+     exit 2;
+   fi
 done
